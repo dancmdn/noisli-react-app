82bb7054b6b747b1b9d34f5ecd840c0953141f89
Danil  Chemodanov <dancmdn@MacBook-Air-Danil.local>
2021-11-24 18:03:55 +0300

    Updates

---
+++ precache-manifest.656fa81eca954c07a47fa6cd5331d273.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5abd8027de708fae0ef801eb480f469",
+    "url": "/noisli-react-app/index.html"
+  },
+  {
+    "revision": "5e77461d528b40259073",
+    "url": "/noisli-react-app/static/css/main.11864c77.chunk.css"
+  },
+  {
+    "revision": "f608d0d1c7d045ba3edf",
+    "url": "/noisli-react-app/static/js/2.12258ee5.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/noisli-react-app/static/js/2.12258ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e77461d528b40259073",
+    "url": "/noisli-react-app/static/js/main.c8b739d7.chunk.js"
+  },
+  {
+    "revision": "dc4f6b5f0b253bb582d4",
+    "url": "/noisli-react-app/static/js/runtime-main.9abd3307.js"
+  },
+  {
+    "revision": "5dcbf34763ed24ddae036a86bac1f5fc",
+    "url": "/noisli-react-app/static/media/bonfire.5dcbf347.svg"
+  },
+  {
+    "revision": "f1f68c4333f1ab961dd3b560f33793c8",
+    "url": "/noisli-react-app/static/media/bonfire.f1f68c43.mp3"
+  },
+  {
+    "revision": "b5bce05387fe4f3ed05781696e5a1c3e",
+    "url": "/noisli-react-app/static/media/cicadas.b5bce053.mp3"
+  },
+  {
+    "revision": "e0a65e20554973da13d65401c6855d81",
+    "url": "/noisli-react-app/static/media/cicadas.e0a65e20.svg"
+  },
+  {
+    "revision": "25e93956f3b3f3501f854eecc9705613",
+    "url": "/noisli-react-app/static/media/cityspace.25e93956.svg"
+  },
+  {
+    "revision": "2616c3d1bff5108ea876049393f9cf8f",
+    "url": "/noisli-react-app/static/media/cityspace.2616c3d1.mp3"
+  },
+  {
+    "revision": "147418b2d3a15de32b8933ac735f7765",
+    "url": "/noisli-react-app/static/media/coffee-shop.147418b2.svg"
+  },
+  {
+    "revision": "50b130a9002c7ffa657af484dd6be57c",
+    "url": "/noisli-react-app/static/media/coffee-shop.50b130a9.mp3"
+  },
+  {
+    "revision": "39f91b5ccd1fd448959c4e84d0109324",
+    "url": "/noisli-react-app/static/media/fireplace.39f91b5c.mp3"
+  },
+  {
+    "revision": "85b32c30db9d39bced03a677ae6a7fae",
+    "url": "/noisli-react-app/static/media/fireplace.85b32c30.svg"
+  },
+  {
+    "revision": "56605d1b05f37dbfff306b1d4063c3e7",
+    "url": "/noisli-react-app/static/media/forest.56605d1b.svg"
+  },
+  {
+    "revision": "703ec7e9ad6d0e3871dbc50dd25c5176",
+    "url": "/noisli-react-app/static/media/forest.703ec7e9.mp3"
+  },
+  {
+    "revision": "c2c206927489dc7273559a5214f64123",
+    "url": "/noisli-react-app/static/media/rain-on-tent.c2c20692.svg"
+  },
+  {
+    "revision": "d248c73fda09863455b5dcf4e24489e0",
+    "url": "/noisli-react-app/static/media/rain-on-tent.d248c73f.mp3"
+  },
+  {
+    "revision": "4a92fecd72ac7e1f77191df94feff746",
+    "url": "/noisli-react-app/static/media/rain.4a92fecd.svg"
+  },
+  {
+    "revision": "cf30ac894427ed6d29fd382e5699cc1f",
+    "url": "/noisli-react-app/static/media/rain.cf30ac89.mp3"
+  },
+  {
+    "revision": "d04e152b896a09218015e9ec637a2e19",
+    "url": "/noisli-react-app/static/media/seaside.d04e152b.mp3"
+  },
+  {
+    "revision": "e1f8ac47b5e60b372d3a059cc7340c7b",
+    "url": "/noisli-react-app/static/media/seaside.e1f8ac47.svg"
+  },
+  {
+    "revision": "a19528da27fdca3b089525274e889545",
+    "url": "/noisli-react-app/static/media/summer-night.a19528da.svg"
+  },
+  {
+    "revision": "d68930901bf6ea48bee3855b87568e6f",
+    "url": "/noisli-react-app/static/media/summer-night.d6893090.mp3"
+  },
+  {
+    "revision": "0ab031345393f607563c3681ab52e14a",
+    "url": "/noisli-react-app/static/media/thunder.0ab03134.svg"
+  },
+  {
+    "revision": "1eb4fc6009ddc4c3d76078c77e84b34b",
+    "url": "/noisli-react-app/static/media/thunder.1eb4fc60.mp3"
+  },
+  {
+    "revision": "7180099bdf9fd6e7a0de18087e04314b",
+    "url": "/noisli-react-app/static/media/train.7180099b.svg"
+  },
+  {
+    "revision": "721cf6bbf2b333a02e8e62d7be2aad34",
+    "url": "/noisli-react-app/static/media/train.721cf6bb.mp3"
+  },
+  {
+    "revision": "19ff2687742c9ee33076a9824a423d3f",
+    "url": "/noisli-react-app/static/media/underwater.19ff2687.svg"
+  },
+  {
+    "revision": "51728d92584da0a241c09faa59cf9bf9",
+    "url": "/noisli-react-app/static/media/underwater.51728d92.mp3"
+  },
+  {
+    "revision": "8e56fa597b186dd3a9a714570f4f1a96",
+    "url": "/noisli-react-app/static/media/water-stream.8e56fa59.mp3"
+  },
+  {
+    "revision": "94bbf000c41589f9ab6843e586de685e",
+    "url": "/noisli-react-app/static/media/water-stream.94bbf000.svg"
+  },
+  {
+    "revision": "cce7f2b9302bbc824f0526c0d4a4bba6",
+    "url": "/noisli-react-app/static/media/waterfall.cce7f2b9.svg"
+  },
+  {
+    "revision": "f5d7b92780f2fe9fcf73008ba050bd96",
+    "url": "/noisli-react-app/static/media/waterfall.f5d7b927.mp3"
+  },
+  {
+    "revision": "14544eef61d58a340a77015586bb5020",
+    "url": "/noisli-react-app/static/media/wind.14544eef.svg"
+  },
+  {
+    "revision": "5558cbb55c26b9dbf4d34aa0e0b0fbe9",
+    "url": "/noisli-react-app/static/media/wind.5558cbb5.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/noisli-react-app/precache-manifest.6003a9b20c9bc46bd5a7a98f43df7f4a.js"
+  "/noisli-react-app/precache-manifest.656fa81eca954c07a47fa6cd5331d273.js"
 );
 
 self.addEventListener('message', (event) => {
